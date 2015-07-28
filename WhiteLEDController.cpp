da3488d9bbce1c9814a09c651cc6d9fb7fd088f1
maeganw12 <maeganjwilson@gmail.com>
2015-07-27 21:06:05 -0600

    Constant variable address

--- WhiteLEDController.cpp
+++ WhiteLEDController.cpp
@@ -3,11 +3,15 @@
 	Address is LOCKED at 1
 	1 parameter--Uses 1 channel of DMX
 */
+
+const int address = 1; //Assigns address as 1
+	//change this value if you want it to work
+	//on a different address 
 void setup()
 {
 
 }
 void loop()
 {
-	
+
 }
